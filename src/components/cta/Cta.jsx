6a7737bb33ee451c55e96c84187dcdebbc0043f2
Vitor <vitor.andreylopess@gmail.com>
2023-03-07 08:44:30 -0300

    terminei a section cta ta mas faltando possibility que ta no pc do senac

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import styles from "./Cta.module.css";
+import "./Cta.css";
 
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <div className="gpt3__cta max_width">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Blog.module.css";
+import "./Blog.css";
 
 const Blog = () => {
   return <div>Blog</div>;
